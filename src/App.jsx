8ac7819452b56249f11f4a93abac25b99a881edc
Hayden Indiana Holdsworth <IndianaHoldsworth@gmail.com>
2022-02-21 18:11:01 -0700

    Add browser router and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
+import { Switch } from 'react-router-dom';
+import { Route } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
+import PrivateRoute from './components/PrivateRoute/PrivateRoute';
+import Auth from './view/Auth/Auth';
 import Home from './view/Home';
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <BrowserRouter>
+        <Switch>
+          <Route path="/auth">
+            <Auth />
+          </Route>
+          <PrivateRoute exact path="/">
+            <Home />
+          </PrivateRoute>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
