0f03b9bda1f3b24ff2577e22ccac0c91dc140a45
Hayden Indiana Holdsworth <IndianaHoldsworth@gmail.com>
2022-02-21 18:15:32 -0700

    Auth Form skeleton

--- src/components/AuthForm/AuthForm.jsx
+++ src/components/AuthForm/AuthForm.jsx
@@ -1,5 +1,17 @@
 import React from 'react';
 
 export default function AuthForm() {
-  return <div>AuthForm</div>;
+  return (
+    <div>
+      <form>
+        <label>Email</label>
+        <input type="text" aria-label="email-input" />
+
+        <label>Password</label>
+        <input type="text" aria-label="password-input" />
+
+        <button type="submit">Sign In</button>
+      </form>
+    </div>
+  );
 }
